b09fe7bff71ee0e5dda8858f11ad4db106af0935
GitOutATown <richard@aqume.com>
2014-05-10 20:36:23 -0700

    backup

---
+++ misc/MultiplicationLab2.scala
@@ -0,0 +1,22 @@
+package misc
+
+object MultiplicationLab2 {
+
+    def main(args: Array[String]): Unit = {
+        
+        for(i <- 1 to 10) {
+            for(j <- 1 to 10) {
+                val prod = (i*j).toString
+                var k = prod.length
+				while (k < 4) {
+					print(" ")
+					k += 1
+				}
+                print(prod)
+            }
+            println()
+        }
+        
+    }
+
+}
