b6275adb54b65e614e46f64af700b4bfedc880ca
GitOutATown <richard@aqume.com>
2014-05-15 13:31:27 -0700

    backup

--- lab/calculus/derivatives/DerivativesLab2.scala
+++ lab/calculus/derivatives/DerivativesLab2.scala
@@ -9,28 +9,30 @@ object DerivativesLab2 {
         def f1 = (x: Double) => pow(x,2)
         def f2 = (x: Double) => pow(x,2) * 1.5
         
-        // input
+        // some input
         var x1 = 3
         var x2 = 7
         
-        // results
-        println("slope for f1 with x1=" + x1 + " and x2=" + x2 + " is " + 
-                slope(f1, x1, x2))
-        println("slope for f2 with x1=" + x1 + " and x2=" + x2 + " is " + 
-                slope(f2, x1, x2))
+        // derivate results
+        println("derivative for f1 with x1=" + x1 + " and x2=" + x2 + " is " + 
+                deriv(f1, x1, x2))
+        println("derivative for f2 with x1=" + x1 + " and x2=" + x2 + " is " + 
+                deriv(f2, x1, x2))
         
-        // new input
+        // some different input
         x1 = 5
         x2 = 12
         
-        // new results
-        println("slope for f1 with x1=" + x1 + " and x2=" + x2 + " is " + 
-                slope(f1, x1, x2))
-        println("slope for f2 with x1=" + x1 + " and x2=" + x2 + " is " + 
-                slope(f2, x1, x2))
+        // new derivative results
+        println("derivative for f1 with x1=" + x1 + " and x2=" + x2 + " is " + 
+                deriv(f1, x1, x2))
+        println("derivative for f2 with x1=" + x1 + " and x2=" + x2 + " is " + 
+                deriv(f2, x1, x2))
     }
     
-    def slope(f: Double => Double, x1: Double, x2: Double): Double = {
+    
+    
+    def deriv(f: Double => Double, x1: Double, x2: Double): Double = {
         val dx = delta(x1, x2)
         (f(x1 + dx) - f(x1)) / dx
     }
