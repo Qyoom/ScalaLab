90783c69518acf9c66788de59978d331c9ad37ba
GitOutATown <richard@aqume.com>
2014-10-13 21:21:17 -0700

    Have working collect case pattern matching on RequiredPermissions

--- lab/case_classes_pattern_matching/authorize/Authorize_lab_3.scala
+++ lab/case_classes_pattern_matching/authorize/Authorize_lab_3.scala
@@ -1,6 +1,6 @@
 package lab.case_classes_pattern_matching.authorize
 
-object Authorize_lab_3 {
+object Authorize_lab_3 extends App {
     
 	val rp1 = RequiredPermission(1L, Some(11L), "project/update", "ORG_ADMIN", None)
 	val rp2 = RequiredPermission(2L, None, "project/delete", "ORG_ADMIN", None)
@@ -8,26 +8,11 @@ object Authorize_lab_3 {
 	val rp4 = RequiredPermission(4L, Some(13L), "project/update", "ORG_ADMIN", Some("lang=EN"))
 	
 	val requiredPerms = List(rp1, rp2, rp3, rp4)
-		
-	def authorize (rp: RequiredPermission): Option[RequiredPermission] = rp match {
-	    case RequiredPermission(id, Some(projectId), "project/update", "ORG_ADMIN", rule) => {
-            if (projectId == 13) {
-                println("Got id: " + id)
-                Some(rp)
-            }
-            None
-	    }
-	    case _ => {
-	        println("Not what I'm looking for")
-	        None
-	    }
-	}
+			
+	val relevantReqPerms = requiredPerms.collect { rp => rp match {
+	    case RequiredPermission(id, Some(projectId), "project/update", "ORG_ADMIN", rule) 
+	        if (projectId == 13) => rp
+	}}
+	relevantReqPerms.foreach(println)
 	
-	val relevantReqPerms = for (rp <- requiredPerms) yield {
-	    rp match {
-	        case RequiredPermission(id, Some(projectId), "project/update", "ORG_ADMIN", rule) if (projectId == 13) => {
-	            rp
-	        }
-	    }
-	}
 }
